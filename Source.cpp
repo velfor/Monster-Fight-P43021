d2ff2c802afa9c2d212ccbd01b2872f591ea3c35
Paul Nikiforoff <velfor@tut.by>
2021-12-21 18:21:17 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <string>
+#include <cstdlib> // для rand() и srand()
+#include <ctime> // для time()
 #include "creature.h"
+#include "player.h"
+#include "monster.h"
+
 using namespace std;
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0))); // устанавливаем значение системных часов в качестве стартового числа
+	rand(); // сбрасываем первый результат
+
+	for (int i = 0; i < 10; ++i)
+	{
+		Monster m = Monster::getRandomMonster();
+		std::cout << "A " << m.getName() << " was created.\n";
+	}
+
+	return 0;
+}
 
 
--- monster.h
+++ monster.h
@@ -1,13 +1,30 @@
 #pragma once
 #include "creature.h"
+#include <ctime>
 
 class Monster : public Creature {
 
 public:
 	enum Type { SLIME, ORC, DRAGON, MAX_TYPE };
 	struct MonsterData {
-		
+		string name;
+		int hp;
+		int damage;
 	};
-	//Monster(Type type) :Creature{}
+	static MonsterData monster_data[MAX_TYPE];
 
+	Monster(Type type) :Creature(monster_data[type].name, monster_data[type].hp,
+		monster_data[type].damage)
+	{}
+	static Monster getRandomMonster() {
+		//srand(time(0));
+		int tmp = rand() % 3;
+		return Monster(static_cast<Type>(tmp));
+	}
+
+};
+Monster::MonsterData Monster::monster_data[Monster::MAX_TYPE]{
+		{"slime",1,1},
+		{"orc",4,2},
+		{"dragon",20,4}
 };
